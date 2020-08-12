2700892ace5ad21dde403dad26327a2099c440a2
JorgeZarlenga <jorge.zarlenga@outlook.com>
2020-08-12 12:07:59 -0300

    Configuração do slider utilizando o padrão da dependência

--- app/src/main/java/com/jorge/projetoslider/MainActivity.java
+++ app/src/main/java/com/jorge/projetoslider/MainActivity.java
@@ -4,11 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                                .title("Título")
+                                .description("Descrição")
+                                .image(R.drawable.um)
+                                .background(android.R.color.holo_orange_light)
+                                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                                .title("Título 2")
+                                .description("Descrição 2")
+                                .image(R.drawable.dois)
+                                .background(android.R.color.holo_orange_light)
+                                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 3")
+                .description("Descrição 3")
+                .image(R.drawable.tres)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
     }
 }
